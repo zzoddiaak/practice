974119a2791a0048cc60a8ef9ee594d53192431b
Даниил Савко <zzoddiaak@gmail.com>
2026-02-17 15:22:45 +0300

    Update Cell

--- src/main/java/untitled/task2/entity/Cell.java
+++ src/main/java/untitled/task2/entity/Cell.java
@@ -5,27 +5,23 @@ import lombok.Getter;
 @Getter
 public class Cell {
     private final int number;
-    private boolean occupied;
     private String orderId;
 
     public Cell(int number) {
         this.number = number;
-        this.occupied = false;
         this.orderId = null;
     }
 
     public boolean isFree() {
-        return !occupied;
+        return orderId == null;
     }
 
     public void placeOrder(String orderId) {
-        this.occupied = true;
         this.orderId = orderId;
     }
 
     public String removeOrder() {
         String removedOrder = this.orderId;
-        this.occupied = false;
         this.orderId = null;
         return removedOrder;
     }
